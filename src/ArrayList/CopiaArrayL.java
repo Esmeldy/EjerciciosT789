19acfbe118608eccdf132ca2f21a544e29b1eaf8
Esmeldy <esmeldyfm@hotmail.com>
2022-09-08 11:51:18 +0200

    Terminado el ejercicio 5

--- src/ArrayList/CopiaArrayL.java
+++ src/ArrayList/CopiaArrayL.java
@@ -13,14 +13,17 @@ public class CopiaArrayL {
         nombres.add("Marta");
         nombres.add("Laura");
 
+        System.out.println("ArrayList: ");
+        for (String n : nombres){
+            System.out.println(n);
+        }
 
         LinkedList<String> nombresLinked = new LinkedList<>();
+        nombresLinked.add(nombres.get(0));
+        nombresLinked.add(nombres.get(1));
+        nombresLinked.add(nombres.get(2));
+        nombresLinked.add(nombres.get(3));
 
-        //recorrer y copiar en LinkedList
-        for (String n : nombres) {
-            nombresLinked.add(n);
-            System.out.println(n); //imprime ArrayList
-        }
         System.out.println("\nLinkedList: ");
         for (String m : nombresLinked){
             System.out.println(m);
