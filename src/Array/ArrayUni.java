6e918527c25323494203e00f85f6174dd9f8b466
Esmeldy <esmeldyfm@hotmail.com>
2022-09-08 10:28:37 +0200

    Terminado el ejercicio 1

---
+++ src/Array/ArrayUni.java
@@ -0,0 +1,13 @@
+package Array;
+
+public class ArrayUni {
+    public static void main(String[] args) {
+        //Crear array unidimensional
+        String[] array = {"Pedro", "Jorge", "Marta", "Merche", "Paula", "Pablo", "Karina"};
+
+        //recorrer el array unidimensional
+        for (String valor: array) {
+            System.out.println(valor);
+        }
+    }
+}
--- src/Array/FunctionReverse.java
+++ src/Array/FunctionReverse.java
@@ -1,15 +1,15 @@
 package Array;
 
-public class FuntionReverse {
+public class FunctionReverse {
     public static void main(String[] args) {
         System.out.println(Reverse("Hola mundo"));
     }
     public static String Reverse(String cadena){
-        String txt = "";
+        StringBuilder txt = new StringBuilder();
         for (int i = 0; i < cadena.length(); i++) {
-            txt += cadena.charAt(cadena.length() - i - 1);
+            txt.append(cadena.charAt(cadena.length() - i - 1));
         }
 
-        return txt;
+        return txt.toString();
     }
 }
